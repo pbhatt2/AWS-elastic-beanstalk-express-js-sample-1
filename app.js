3851bab657f99ad945e81a3a8fb2d90c4add7095
p.bhatt2 <p.bhatt2@postgrad.curtin.edu.au>
2022-09-28 10:19:34 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Parin sample - changes 2'));
+app.get('/', (req, res) => res.send('Parin sample - changes 3- pipeline test'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
